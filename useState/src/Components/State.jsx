2445bfcc78ac84808d5dfa4a3cfc0a4bc02bd991
Dev_Jadav <jadavdevendrasinh7@gmail.com>
2025-06-03 00:22:59 +0530

    Use State

--- useState/src/Components/State.jsx
+++ useState/src/Components/State.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import { useState } from 'react'
 
 export default function State() {
-    const [count, setCount] = useState(0)
+    // const [count, setCount] = useState(0)
+    // const [str,setStr] = useState("Dev")
+    // const [arr,setArr] = useState([10,20,30,40,50])
+    const [bool,setBool] = useState("true")
+
+    const update = () => {
+      // const newarr = arr.map((e) => {
+      //   return e + 10
+      // })
+      // setArr(newarr)
+
+      // (bool == "true")?setBool("false"):setBool("true")
+    }
   return (
-    <div>
-    </div>
+    <>
+      {/* {arr.map((e) => {
+        return(
+          <p>{e}</p>
+        )
+      })} */}
+
+      <p>{bool}</p>
+      <button onClick={update}>Update</button>
+    </>
   )
 }
